b20a174818f6c0bd300939ba1362eaf180a0661d
NaveenjothiP <36544639+naveenjothi@users.noreply.github.com>
2024-09-04 12:12:48 +0530

    updated base model

---
+++ internal/model/user_model.go
@@ -0,0 +1,16 @@
+package model
+
+type Address struct{
+	City string `json:"city" bson:"city"`
+	Country string `json:"country" bson:"country"`
+}
+
+type UserModel struct{
+	FirstName string `json:"firstName" bson:"firstName"`
+	LastName string `json:"lastName" bson:"lastName"`
+	Mobile string `json:"mobile" bson:"mobile"`
+	Email string `json:"email" bson:"email"`
+	ProfileName string `json:"profileName" bson:"profileName"`
+	Address Address `json:"address" bson:"address"`
+	BaseModel
+}
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,18 +5,7 @@ import (
 )
 
 func (s *FiberServer) RegisterFiberRoutes() {
-	s.App.Get("/", s.HelloWorldHandler)
-
 	s.App.Get("/health", s.healthHandler)
-
-}
-
-func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
-	resp := fiber.Map{
-		"message": "Hello World",
-	}
-
-	return c.JSON(resp)
 }
 
 func (s *FiberServer) healthHandler(c *fiber.Ctx) error {
