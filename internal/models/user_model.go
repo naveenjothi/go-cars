d15fdfeb99f8c5035eca95705e87f67ba42ea988
NaveenjothiP <36544639+naveenjothi@users.noreply.github.com>
2024-09-12 23:43:21 +0530

    updated user model

--- internal/models/user_model.go
+++ internal/models/user_model.go
@@ -1,16 +1,21 @@
 package models
 
-type Address struct{
-	City string `json:"city" bson:"city"`
-	Country string `json:"country" bson:"country"`
+type Address struct {
+	City      string `json:"city" bson:"city"`
+	CityId    string `json:"cityId" bson:"cityId"`
+	State     string `json:"state" bson:"state"`
+	CountryId string `json:"countryId" bson:"countryId"`
+	Country   string `json:"country" bson:"country"`
 }
 
-type UserModel struct{
-	FirstName string `json:"firstName" bson:"firstName"`
-	LastName string `json:"lastName" bson:"lastName"`
-	Mobile string `json:"mobile" bson:"mobile"`
-	Email string `json:"email" bson:"email"`
-	ProfileName string `json:"profileName" bson:"profileName"`
-	Address Address `json:"address" bson:"address"`
-	BaseModel `bson:",inline"`
+type UserModel struct {
+	FirstName        string  `json:"firstName" bson:"firstName"`
+	LastName         string  `json:"lastName" bson:"lastName"`
+	Mobile           string  `json:"mobile" bson:"mobile"`
+	Email            string  `json:"email" bson:"email"`
+	ProfileName      string  `json:"profileName" bson:"profileName"`
+	Address          Address `json:"address" bson:"address"`
+	IsMobileVerified bool    `json:"isMobileVerified" bson:"isMobileVerified"`
+	IsKYCVerified    bool    `json:"isKYCVerified" bson:"isKYCVerified"`
+	BaseModel        `bson:",inline"`
 }
